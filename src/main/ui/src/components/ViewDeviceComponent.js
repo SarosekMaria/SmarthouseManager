7cc1900254b8c205d43fbc64cf108860f0a6f40f
assasin <mashasarosek@yandex.ru>
2020-12-13 15:14:23 +0300

    Add View

--- src/main/ui/src/components/ViewDeviceComponent.js
+++ src/main/ui/src/components/ViewDeviceComponent.js
@@ -1,22 +1,41 @@
 import React, {Component} from "react";
+import DeviceService from "./DeviceService";
 
 class ViewDeviceComponent extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            id: this.props.match.params.id,
+            device: {}
         }
     }
 
     componentDidMount() {
-
+        DeviceService.getDeviceById(this.state.id).then((res) => {
+           this.setState({
+              device: res.data
+           });
+        });
     }
 
     render() {
         return (
             <div>
-                <h2>View Device Page</h2>
+                <br></br>
+                <div className="card col-md-6 offset-md-3">
+                    <h3 className="text-center">View Device Details</h3>
+                    <div className="card-body">
+                        <div className="row">
+                            <label>Device Name:</label>
+                            <div>{this.state.device.name}</div>
+                        </div>
+                        <div className="row">
+                            <label>Device Description:</label>
+                            <div>{this.state.device.description}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
